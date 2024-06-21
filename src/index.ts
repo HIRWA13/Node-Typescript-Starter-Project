cb15061644b435ebb2ab7bdd33c9fd3856a9a1b6
Junior Hirwa <iamhirwejr@gmail.com>
2024-06-21 10:45:49 +0200

    feat: add backend features

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Hello World!', 5 + 10);
+import express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello there this is the base route for the application')
+})
+
+const PORT = 3000
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+})
